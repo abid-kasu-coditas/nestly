df28a09ee141d70c6a0d3df6cb4deafe3778377e
om-lakade-coditas <om.lakade@coditas.com>
2026-07-14 11:30:19 +0530

    initialised login module

--- nestly-frontend/src/components/ui-components/Sidebar/Sidebar.tsx
+++ nestly-frontend/src/components/ui-components/Sidebar/Sidebar.tsx
@@ -1,13 +1,16 @@
+import { Sidebar_items } from '../../../constants/constants'
+import { useAppSelector } from '../../../redux/store/hooks'
 import styles from './Sidebar.module.scss'
 import type { SidebarProps } from './Sidebar.types'
 
 const Sidebar = ({ properties }:SidebarProps) => {
+  type role = "Admin" | "User"
+  const userRole: role = useAppSelector(state => state.authUser.user)
+
   return (
     <div className={styles.Sidebar}>
-        <div>
-            {properties.map((property)=>{
-                return <div>{property}</div>
-            })}
+        <div className={styles.SidebarOptionsContainer}>
+            {Sidebar_items}
         </div>
     </div>
   )
--- nestly-frontend/src/constants/constants.ts
+++ nestly-frontend/src/constants/constants.ts
@@ -4,5 +4,20 @@ export const LogoName = "Nestly";
 export const Content_Text = "A Property Rental and Verification Platform.";
 
 export const Sidebar_items = {
-    
+    Admin : [
+        {
+            users: "/users"
+        },
+        {
+            listings : "/listings"
+        }
+    ],
+    User : [
+        {
+            properties: "/properties"
+        },
+        {
+            Favourites : "/favourites"
+        }
+    ],
 }
